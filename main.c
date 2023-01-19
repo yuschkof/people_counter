1b28ccdee25bc0adb8715f21267bc8a34f35df15
Anton Yushkov <61192109+yuschkof@users.noreply.github.com>
2023-01-19 15:17:06 +0400

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <avr/io.h>		 // Библиотека для работы с портами ввода-вывода
 #include <util/delay.h>	 // Библиотека для работы с паузой
 
-#define WHITE_P PF1 // Приемник Белый
-#define BLUE_P PF2 // Приемник Синий
-#define GREEN_P PF3 // Приемник Зеленый
+#define WHITE_P PE0 // Приемник Белый
+#define BLUE_P PE1// Приемник Синий
+#define GREEN_P PE2 // Приемник Зеленый
 
 #define WHITE_I PB6 // Излучатель Белый
 #define BLUE_I PB5 // Излучатель Синий
@@ -14,42 +14,88 @@
 
 int count=0;
 
-void IN()
-{
-    	count++;
-	_delay_ms(500);
-}
-void OUT()
-{
-  	count--;
-	_delay_ms(500);
-}
 
-
-int main ()
-{
-	DDRF |= (1 << OUTPUT);
-	DDRB |= (1 << WHITE_I) | (1 << BLUE_I) | (1 << GREEN_I);
-	DDRF &= ~((1 << WHITE_P) | (1 << BLUE_P) | (1 << GREEN_P));
-	PORTF |= (1 << WHITE_P) | (1 << BLUE_P) | (1 << GREEN_P);
-	PORTB |= (1 << WHITE_I); // Включаем белый излучатель
-	PORTB |= (1 << GREEN_I); // Включаем зеленый излучатель
-	// Главный цикл работы МК
-	while (1)
-	{
-		
-		if ((PINF & (1 << WHITE_P)) == 0){
-			_delay_ms(100);
-			if ( PINF & ( 1 << GREEN_P)) == 0{
-				IN();
-			}
-		}
-		if ((PINF & (1 << GREEN_P)) == 0){
-			_delay_ms(100);
-			if ( PINF & ( 1 << WHITE_P)) == 0{
-				OUT();
-			}
-		}
-			
+void IN() {
+    count++;
+    /*PORTB |= (1 << WHITE_I);
+	PORTB &= ~(1 << BLUE_I);
+	PORTB |= (1 << GREEN_I);*/
+}
+void OUT() {
+	if(count > 0) {
+		count--;
 	}
+	/*PORTB |= (1 << GREEN_I);
+	PORTB &= ~(1 << BLUE_I);
+	PORTB |= (1 << WHITE_I);*/
+}
+
+
+
+int main () {
+    DDRB |= (1 << WHITE_I) | (1 << BLUE_I) | (1 << GREEN_I);
+    DDRE &= ~((1 << WHITE_P) | (1 << BLUE_P) | (1 << GREEN_P));
+    PORTE |= ((1 << WHITE_P) | (1 << BLUE_P) | (1 << GREEN_P));
+    PORTB |= (1 << WHITE_I); // Включаем белый излучатель
+    PORTB |= (1 << GREEN_I); // Включаем зеленый излучатель
+    DDRF = 0b11111111;
+    // Главный цикл работы МК
+    _delay_ms(500);
+    int a[] = {0b11000000, 0b11111001, 0b10100100, 0b10110000, 0b10011001, 0b10010010, 0b10000010, 0b11111000, 0b10000000, 0b10010000}; //массив цифр от 0 до 9
+    while (1) {
+
+        /*if ((PIND & (1 << WHITE_P)) == 0) {
+			PORTB &= ~(1 << WHITE_I);
+            PORTB |= (1 << BLUE_I);
+            PORTB &= ~(1 << GREEN_I);
+            start = GetLocalTime(&st);
+            while (GetLocalTime(&st) - start < 5){
+				if ((PIND & (1 << BLUE_P)) == 0) {
+					IN();
+					p++;
+					break;
+				}
+			}
+			
+			if (p == 0){
+				PORTB |= (1 << WHITE_I);
+				PORTB &= ~(1 << BLUE_I);
+				PORTB |= (1 << GREEN_I);
+			}
+			p = 0;
+		}
+		
+		if ((PIND & (1 << GREEN_P)) == 0) {
+			PORTB &= ~(1 << GREEN_I);
+			PORTB &= ~(1 << WHITE_I);
+			PORTB |= (1 << BLUE_I);
+			start = time();
+           	while (time() - start < 5){
+				if ((PIND & (1 << BLUE_P)) == 0) {
+					OUT();
+					p++;
+					break;
+				}
+			}
+			
+			if (p == 0){
+				PORTB |= (1 << GREEN_I);
+				PORTB &= ~(1 << BLUE_I);
+				PORTB |= (1 << WHITE_I);
+			}
+			p = 0;
+		}*/
+		if (count >= 0 && count < 10) {
+			PORTF = a[count];
+		}	
+		
+		if ((PINE & (1 << GREEN_P)) == 0) {
+			IN();
+			_delay_ms(500);
+		}
+		else if ((PINE & (1 << WHITE_P)) == 0) {
+			OUT();
+			_delay_ms(500);
+		}
+	}     
 }
